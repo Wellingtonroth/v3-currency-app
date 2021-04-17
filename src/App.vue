6e7d7301b0e73d4b840ee18893dfcf1f6c9ad074
Wellington Roth <wellingtonrothh@gmail.com>
2021-04-17 11:19:03 -0300

    get axios

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
 <template>
   <div class="container grid-lg">
-    <h1>Hello World</h1>
+    <div class="card">
+      <div class="card-header">
+        <div class="h4">Todas as moedas</div>
+      </div>
+      <div class="card-body">
+        
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { onMounted, reactive, toRefs } from 'vue'
+import api from "./services/api"
+
 export default {
   name: 'App',
+  setup() {
+    const data = reactive({
+      quotes: {},
+    })
+
+    onMounted(async () => {
+      const response = await api.all()
+      data.quotes = response.data
+    })
+
+    return { ...toRefs(data) }
+  }
 }
 </script>

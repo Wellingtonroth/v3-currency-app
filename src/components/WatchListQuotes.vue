f57ae8505730ea2effb982728567e03a4a4b1127
Wellington Roth <wellingtonrothh@gmail.com>
2021-04-17 15:41:00 -0300

    table favorite

---
+++ src/components/WatchListQuotes.vue
@@ -0,0 +1,25 @@
+<template>
+  <ListQuotes :quotes="quotes" />
+  <div class="mt-2 text-right">
+    <cite class="text-small">
+      Atualizará novamente em <b>{{ nextUpdateTime }} segundos</b>
+    </cite>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import ListQuotes from './ListQuotes'
+
+export default {
+  components: {
+    ListQuotes
+  },
+  setup() {
+    const quotes = ref({});
+    const nextUpdateTime = ref(30)
+
+    return { quotes, nextUpdateTime };
+  }
+}
+</script>

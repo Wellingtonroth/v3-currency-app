da6877c442e81a5b54e181ff365ac083229b0ad8
Wellington Roth <wellingtonrothh@gmail.com>
2021-04-17 11:32:26 -0300

    ListQuotes component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="container grid-lg">
+  <div class="container grid-lg my-2 py-2">
     <div class="card">
       <div class="card-header">
         <div class="h4">Todas as moedas</div>
       </div>
       <div class="card-body">
-        
+        <ListQuotes :quotes="quotes" />
       </div>
     </div>
   </div>
@@ -14,9 +14,13 @@
 <script>
 import { onMounted, reactive, toRefs } from 'vue'
 import api from "./services/api"
+import ListQuotes from './components/ListQuotes'
 
 export default {
   name: 'App',
+  components: {
+    ListQuotes
+  },
   setup() {
     const data = reactive({
       quotes: {},
